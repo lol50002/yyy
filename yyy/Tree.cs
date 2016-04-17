1c696a58ea34f8c89c014fc9b159862f19848381
lol50002 <artem130500@mail.ru>
2016-04-17 16:55:35 +0300

    cvxcvx
    xcvxcv

---
+++ yyy/Tree.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Tree
+    {
+        string value;
+        Tree Left;
+        Tree Right;
+        public static Tree getTree(string str)
+        {
+            Stack<char> sm = new Stack<char>();
+
+            for (char chr=str[0];chr<str.Length;chr++)
+            {
+                if (chr == '(' || chr == '{' || chr == '[')
+                {
+                    sm.Push(chr);
+                    continue;
+                }
+
+                if (chr != ')' && chr != '}' && chr != ']')
+                    continue;
+
+                char brace;
+
+                if (sm.Count > 0)
+                    brace = (Char)sm.Pop();
+                if ((sm.Count ==0)&&(chr='+'))
+                    return chr='+';
+            }
+            return 
+        }
+
+
+        
+    }
+}
